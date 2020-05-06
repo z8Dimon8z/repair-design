77203a9b29df2765ab79523e45ab6b9592516f33
z8Dimon8z <dima-lysak@yandex.ru>
2020-05-06 18:42:07 +0300

    feat: Добавил gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,29 @@
-const gulp = require('gulp');
+const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
 
-gulp.task('hello', function(done){
-    console.log('Привет мир');
-    done();
-});
+
 
 // Static server
-gulp.task('browser-sync', function() {
+ function bs() {
+    serveSass();
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
-    gulp.watch("./*.html").on('change', browserSync.reload);
-});
+    
+    watch("./sass/**/*.sass", serveSass);
+    watch("./js/*.js").on('change', browserSync.reload);
+    watch("./*.html").on('change', browserSync.reload);
+};
+
+// Compile sass into CSS & auto-inject into browsers
+function serveSass() {
+    return src("./sass/*.sass")
+        .pipe(sass())
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
