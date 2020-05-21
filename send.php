00822997479adabc49cd4b7c9018f5ca2a3d9f57
z8Dimon8z <dima-lysak@yandex.ru>
2020-05-21 13:52:31 +0300

    feat: сделал отправку писем с модального окна

---
+++ send.php
@@ -0,0 +1,39 @@
+<?php
+
+$userName = $_POST['userName'];
+$userEmail = $_POST['userEmail'];
+$userPhone = $_POST['userPhone'];
+
+// Load Composer's autoloader
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'dima.site19822020@gmail.com';                     // SMTP username логин почты сборшика в нашем случае gmail
+    $mail->Password   = '159357zxcqweasd2020';                               // SMTP password пароль почты сборшика в нашем случае gmail
+    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 465;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('dima.site19822020@gmail.com');            // почта сборшика в нашем случае gmail
+    $mail->addAddress('dima-lysak@yandex.ru');     // Add a recipient куда переслать письмо (почта клиента)
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Новая заявка с сайта';
+    $mail->Body    = "Имя пользователя: ${userName}, его телефон: ${userPhone}. Его почта: ${userEmail}";
+
+    $mail->send();
+    header('Location: thanks.html');
+} catch (Exception $e) {
+    echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+}
